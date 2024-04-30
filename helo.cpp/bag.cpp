8b09cb23e4a4704a1eb314b22cc6d8c9d2373ddc
Sandor001 <sandorszpivak@gmail.com>
2024-04-30 17:25:24 +0200

    helo.cpp/bag.cpp

---
+++ helo.cpp/bag.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // This is a comment//
+cout  <<"It there a bug here?/n";
+    return 1 ;
+}
+//Зміна значення return з 0 до 1 все працює//
