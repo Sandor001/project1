f174700219a885634c2275e63c1b0ab4dde97cd2
Sandor001 <sandorszpivak@gmail.com>
2024-05-14 21:20:46 +0200

    :

---
+++ helo.cpp/bug of Func3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int myFunc(unsigned short int x);
+
+int main()
+{
+    unsigned short int x, y;
+    int myFunc (x=y);
+    cout << "x: " << x << " y: " << y << "\n";
+}
+
+int myFunc(unsigned short int x);
+
+    
