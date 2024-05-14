bf82f8c0577411c3277afca7f27c7a186c97252e
Sandor001 <sandorszpivak@gmail.com>
2024-05-14 21:04:14 +0200

    :

---
+++ helo.cpp/bug of Func.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void myFunc(unsigned short int x);
+int main()
+{
+    unsigned short int x, y;
+    int myFunc (x =y);
+    cout << "x: " << x << "y: " << y << "\n";
+}
+void myFunc(unsigned short int x)
+{
+    return ;
+    
+}
