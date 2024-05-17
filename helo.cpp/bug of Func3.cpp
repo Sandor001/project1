4dd1d5529d4f4b45620317ba4b26761549b76dfe
Sandor001 <sandorszpivak@gmail.com>
2024-05-17 18:03:47 +0200

    :helo.cpp/bug of Func3.cpp

--- helo.cpp/bug of Func3.cpp
+++ helo.cpp/bug of Func3.cpp
@@ -5,12 +5,11 @@ int myFunc(unsigned short int x);
 
 int main()
 {
-    unsigned short int x, y;
-    int myFunc (x =y);
+    unsigned short int x=2, y=2;
+    int myFunc (x);
     cout << "x: " << x << " y: " << y << "\n";
     return (4*x);
 }
-
 
  
 
