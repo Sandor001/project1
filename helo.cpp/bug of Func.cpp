2e7189726bcc68e06a2839c2de14c84edc04d21b
Sandor001 <sandorszpivak@gmail.com>
2024-05-17 16:39:16 +0200

    :

--- helo.cpp/bug of Func.cpp
+++ helo.cpp/bug of Func.cpp
@@ -7,9 +7,13 @@ int main()
     unsigned short int x, y;
     int myFunc (x =y);
     cout << "x: " << x << "y: " << y << "\n";
+    return (4*x);
 }
 void myFunc(unsigned short int x)
+
 {
-    return ;
-    
+
+    int myFunc (x);
+    cout << "x: " << x << "y: " << 'y' << "\n";
+     
 }
